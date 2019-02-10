03d230168bf1a0ba0251e72a6b74fda90b188304
josealarconchacon <jose.alarcon.chacom@gmail.com>
2019-02-09 20:15:15 -0500

    Setting map

--- FourCubed/Controller/ViewController.swift
+++ FourCubed/Controller/ViewController.swift
@@ -7,16 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     var venueView = VenueView()
     
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setMapKit()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home Page"
         self.view.backgroundColor = .white
         view.addSubview(venueView)
-       
+    }
+    func setMapKit() {
+        let mapView = MKMapView()
+        
+        let leftMargin:CGFloat = 10
+        let topMargin:CGFloat = 100
+        let mapWidth:CGFloat = view.frame.size.width-20
+        let mapHeight:CGFloat = 300
+        
+        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+        
+        mapView.mapType = MKMapType.standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        // Or, if needed, we can position map in the center of the view
+        mapView.center = view.center
+        
+        view.addSubview(mapView)
     }
 }
