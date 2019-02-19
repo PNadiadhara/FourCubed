bd58688ebddecc404773bdc010f7d0a7885e1ef6
vaslee <ltx0503@gmail.com>
2019-02-19 12:57:58 -0500

    add Venues Model which can save and get data from file, add favorite model.

--- FourCubed/Controller/ListVenueDetailViewController.swift
+++ FourCubed/Controller/ListVenueDetailViewController.swift
@@ -19,11 +19,18 @@ class ListVenueDetailViewController: UIViewController {
 
         view.addSubview(searchDetailView)
         view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
-        
-        
+        let rightButton = UIBarButtonItem(title: "Create", style: UIBarButtonItem.Style.plain, target: self, action: #selector(favoriteButtonPress))
+        self.navigationItem.rightBarButtonItem = rightButton
+        let leftButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: #selector(cancelButtonPress))
+        self.navigationItem.leftBarButtonItem = leftButton
     }
     
 
-   
-
+    @objc func favoriteButtonPress() {
+        
+    }
+    
+    @objc func cancelButtonPress() {
+        dismiss(animated: true)
+    }
 }
